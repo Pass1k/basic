81c53af2f5229bad72551e5a8d71415331de7f64
Pasik <parasatto@gmail.com>
2022-12-22 23:13:32 +0600

    2 commit

---
+++ practic 3.7/task_3.py
@@ -0,0 +1,7 @@
+print('Задача 3. Следующее и предыдущее')
+
+number = int(input('Введите число: '))
+next_number = number + 1
+previous_number = number - 1
+print(f'После числа {number} идёт число {next_number}')
+print(f'До числа {number} идёт число {previous_number}')
